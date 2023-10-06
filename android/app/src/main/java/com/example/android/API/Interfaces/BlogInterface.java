ff634264b05a0e60d80ad4efcd6e7797d3bb89ab
Ayush Shrivastav <ayush41red@gmail.com>
2023-10-06 16:42:04 +0530

    Implemented Interfaces for User, Comments and Blogs

---
+++ android/app/src/main/java/com/example/android/API/Interfaces/BlogInterface.java
@@ -0,0 +1,6 @@
+package com.example.android.API.Interfaces;
+
+public interface BlogInterface {
+    void editTitle(String newTitle);
+    void editContent(String newContent);
+}
---
+++ android/app/src/main/java/com/example/android/API/Interfaces/CommentInterface.java
@@ -0,0 +1,5 @@
+package com.example.android.API.Interfaces;
+
+public interface CommentInterface {
+    public void editComment(String newComment);
+}
