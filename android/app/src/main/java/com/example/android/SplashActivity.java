5089537d7e6955d8bc70bb642bb923304ba33326
Ayush Shrivastav <ayush41red@gmail.com>
2023-10-09 15:08:25 +0530

    Add component and animations to android code

--- android/app/src/main/java/com/example/android/SplashActivity.java
+++ android/app/src/main/java/com/example/android/SplashActivity.java
@@ -1,16 +1,47 @@
 package com.example.android;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 @SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
+
+    private final String logoText = "Expresso";
+    private TextView logoTextView;
+    private int charIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        logoTextView = findViewById(R.id.appName);
+
+        animateTextTypewriter();
+    }
+
+    private void animateTextTypewriter() {
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (charIndex <= logoText.length()) {
+                    logoTextView.setText(logoText.subSequence(0, charIndex));
+                    charIndex++;
+                    handler.postDelayed(this, 200);
+                } else {
+                    handler.postDelayed(() -> {
+                        Intent onboardingActivity = new Intent(getApplicationContext(), OnboardingActivity.class);
+                        startActivity(onboardingActivity);
+                        finish();
+                    }, 1000);
+                }
+            }
+        }, 200);
     }
 }
