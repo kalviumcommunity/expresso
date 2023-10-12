dcad7c89c5e960ed25b45cc8635b4b45234f9ffc
Ayush Shrivastav <ayush41red@gmail.com>
2023-10-12 21:34:08 +0530

    Added packages for backend

--- server/server.js
+++ server/server.js
@@ -2,14 +2,18 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const userRouter = require("./controllers/UserController");
+const categoryRouter = require("./controllers/CategoryController");
+const blogsController = require("./controllers/BlogsController");
+const bodyParser = require("body-parser");
 app.use(cors());
-
+app.use(express.json());
+app.use(bodyParser.json());
 const PORT = process.env.PORT;
 
-app.get("/", (req, resp) => {
-  console.log(req.path);
-  resp.json({ greeting: "Hello User" });
-});
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/category", categoryRouter);
+app.use("/api/v1/blogs", blogsController);
 
 app.listen(PORT, () => {
   console.log(`Listening on Port:${PORT}`);
