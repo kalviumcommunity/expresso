fd6c6715b7a82e0017d3c08741f18a57d31d211d
Ayush Shrivastav <ayush41red@gmail.com>
2023-10-13 11:00:17 +0530

    Add server.js

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 const userRouter = require("./controllers/UserController");
 const categoryRouter = require("./controllers/CategoryController");
 const blogsController = require("./controllers/BlogsController");
+const commentsController = require("./controllers/CommentsController");
+const tokenController = require("./controllers/TokenController");
 const bodyParser = require("body-parser");
 app.use(cors());
 app.use(express.json());
@@ -14,6 +16,8 @@ const PORT = process.env.PORT;
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/category", categoryRouter);
 app.use("/api/v1/blogs", blogsController);
+app.use("/api/v1/comments", commentsController);
+app.use("/api/v1/token", tokenController);
 
 app.listen(PORT, () => {
   console.log(`Listening on Port:${PORT}`);
