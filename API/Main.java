ca7ea2983172b54546c0c342f93295625b71bc05
Ayush Shrivastav <ayush41red@gmail.com>
2023-11-06 00:10:46 +0530

    Changed few things in main.java file

--- API/Main.java
+++ API/Main.java
@@ -5,11 +5,10 @@ import org.example.API.Classes.User;
 
 public class Main {
     public static void main(String[] args) {
-        User[] user = new User[3];
-        user[1] = new User("Ayush6041", "ayush.shrivastav@kalvium.com", "Ayush41red", 12345678);
-        user[2] = new User("Sidhant", "siddhant9504@gmail.com", "sidd9504", 1245785);
-        user[0] = new User("Akanksha", "akanksha23123@gmail.com", "vanshbishnoi0029", 45612378);
-//        System.out.println(user[2].getUsername());
-        System.out.println(user[1].getCountUsers());
+        User User1 = new User("Ayush", "ayush.shrivastav@kalvium.com", "Ayush41red", 12345678);
+        User User2 = new User("Sidhant", "siddhant9504@gmail.com", "sidd9504", 1245785);
+        User User3 = new User("Akanksha", "akanksha23123@gmail.com", "vanshbishnoi0029", 45612378);
+//        System.out.println(User1.getUsername() + " " + User2.getUsername() + " " + User3.getUsername());
+        System.out.println(User3.getCountUsers());
     }
 }
